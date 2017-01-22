0e54dcf787b8f6ef2039cf5f94884611ffe54ddf
dusthand <zengruih@qq.com>
2017-01-22 20:30:49 +0800

    1

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
-#	-*-	coding:utf-8	-*-
+#-*-coding:utf-8-*-
 
 import tushare as ts
 
-# ts.get_latest_news() #默认获取最近80条新闻数据，只提供新闻类型、链接和标题
-# df = ts.get_latest_news(top=10) #显示最新5条新闻，并打印出新闻内容
+
+# ts.get_latest_news()
+# df = ts.get_latest_news(top=10) 
 # print df
 # df.to_csv("get_latest_news.csv",encoding="gbk")
 # print ts.latest_content.__doc__
-a = ts.latest_content("http://finance.sina.com.cn/roll/2017-01-22/doc-ifxzutkf2272374.shtml")
-b = repr(a)
-b2 = eval(b[1:])
-print b2
+
+import numpy
+print numpy.version.full_version
